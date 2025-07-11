e12d9536d72b0d0cc0cf2059df255aed54d713d0
ciaranglansford <ciaran.glansford@gmail.com>
2025-07-11 07:54:08 +0100

    Wallet connection displays in nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { WalletConnectComponent } from './shared/component/wallet-connect/wallet-connect.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule],
+  imports: [CommonModule, RouterOutlet, RouterModule, WalletConnectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
